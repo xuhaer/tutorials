d19127d0ac34c2fdf635927cc8a8c10708c7621a
hardly <trigal@qq.com>
2018-06-25 16:08:37 +0800

    add test

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-        
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
