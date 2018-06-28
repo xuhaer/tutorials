2367a136059352d729d2d82ab3b77c279fa31e78
hardly <trigal@qq.com>
2018-06-28 15:10:01 +0800

    add templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id) #使用get_object_or_404以降低模型层和视图层之间的耦合性
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+    
