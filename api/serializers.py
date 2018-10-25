81a90b5f301227175573549ab9c2e299727adbe3
hardly <trigal@qq.com>
2018-10-25 11:32:13 +0800

    add url field in serializers

--- api/serializers.py
+++ api/serializers.py
@@ -3,14 +3,15 @@ from api.models import Question, Choice
 
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
-    
+
     class Meta:
         '''using the ModelSerializer class to refactor serializer '''
         model = Question
-        fields = ('question_text', 'pub_date')
+        fields = ('url', 'question_text', 'pub_date')
+
 
 class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Choice
-        fields = ('question', 'choice_text', 'votes')
+        fields = ('url', 'question', 'choice_text', 'votes')
