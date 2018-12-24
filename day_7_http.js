b3e74b69f2c1b0e6e5554ee752e6fc3f494102a1
hardly <trigal@qq.com>
2018-12-24 16:12:06 +0800

    add http server

---
+++ day_7_http.js
@@ -0,0 +1,25 @@
+/*
+Node.js开发的目的就是为了用JavaScript编写Web服务器程序。
+而要开发HTTP服务器程序，从头处理TCP连接，解析HTTP是不现实的。这些工作实际上已经由Node.js自带的http模块完成了。应用程序并不直接和HTTP协议打交道，而是操作http模块提供的request和response对象。
+用Node.js实现一个HTTP服务器程序非常简单。我们来实现一个最简单的Web程序:http.js
+*/
+'use strict';
+
+// 导入http模块:
+var http = require('http');
+
+// 创建http server，并传入回调函数:
+var server = http.createServer(function (request, response) {
+    // 回调函数接收request和response对象,
+    // 获得HTTP请求的method和url:
+    console.log(request.method + ': ' + request.url);
+    // 将HTTP响应200写入response, 同时设置Content-Type: text/html:
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    // 将HTTP响应的HTML内容写入response:
+    response.end('<h1>Hello world!</h1>');
+});
+
+// 让服务器监听8081端口:
+server.listen(8081);
+
+console.log('Server is running at http://127.0.0.1:8081/');
