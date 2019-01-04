64774075747dbd9e21d52704974a5bed962dcb79
hardly <trigal@qq.com>
2019-01-04 11:03:42 +0800

    refactor: make Flowable be subproject of JavaTutorial

--- BashTaskTest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Hello World !"
-mkdir /Users/har/py/git/FlowableTutorial/BashTaskDir
---
+++ FlowableTutorial/BashTaskTest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Hello World !"
+mkdir /Users/har/git/JavaTutorial/FlowableTutorial/BashTaskDir
